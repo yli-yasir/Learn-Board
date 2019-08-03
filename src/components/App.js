101c09a9a66c468d10ea91d5f9ecb9ef17b211cd
yli-yasir <35302828+yli-yasir@users.noreply.github.com>
2019-08-03 20:45:04 +0300

    init

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Header,Footer} from './layout'
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
