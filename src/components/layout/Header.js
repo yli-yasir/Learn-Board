7cdbec031075111e686c63cd28684f2936155a63
yli-yasir <35302828+yli-yasir@users.noreply.github.com>
2019-08-04 12:23:02 +0300

    work on AppBar

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,2 +1,53 @@
-import React from 'react'
-export default props => { return <p>Header</p>}
+import React from "react";
+import { AppBar, Toolbar, Typography, InputBase } from "@material-ui/core";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import SearchIcon from "@material-ui/icons/Search";
+
+const useStyles = makeStyles(theme => ({
+  searchContainer: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: theme.palette.grey[300],
+    "&:hover": {
+      backgroundColor: fade(theme.palette.grey[300], 0.7)
+    },
+    marginLeft: theme.spacing(2),
+  },
+  searchIconContainer: {
+    position: "absolute",
+    width: theme.spacing(7),
+    display: "flex",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  inputRoot: {},
+  input: { padding: theme.spacing(1, 1, 1, 7)},
+
+}))
+
+export default function Header(props) {
+  const classes = useStyles();
+  return (
+    <AppBar  color="default">
+      <Toolbar>
+        <Typography variant="h6" noWrap>
+          Learnboard
+        </Typography>
+        <div className={classes.searchContainer}>
+          <div className={classes.searchIconContainer}>
+            <SearchIcon />
+          </div>
+          <InputBase
+            placeholder="Search…"
+            classes={{
+              root: classes.inputRoot,
+              input: classes.input
+            }}
+            inputProps={{ "aria-label": "search" }}
+          />
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+}
